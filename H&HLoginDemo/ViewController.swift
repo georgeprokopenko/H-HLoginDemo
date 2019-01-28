f40f2114209b7d62f4163c35b2bf9dec85ec0603
George Prokopenko <green.mobile@mail.ru>
2019-01-28 21:22:27 +0300

    Initial Commit

---
+++ H&HLoginDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  H&HLoginDemo
+//
+//  Created by George Prokopenko on 28/01/2019.
+//  Copyright © 2019 George Prokopenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
